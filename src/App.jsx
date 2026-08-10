5a8f242649abcea7c0e36686d07e741cb7e0d87a
Alek-Grujic <alek.grujic001@outlook.com>
2026-08-10 08:47:34 +0200

    feat: add initial DateCounter component with useState

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import heroImg from "./assets/hero.png";
+import DateCounter from "./DateCounter.jsx";
+
+function App() {
+  return (
+    <div>
+      <DateCounter />
+    </div>
+  );
+}
+
+export default App;
