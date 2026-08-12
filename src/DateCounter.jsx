5c515665d62763ddbba6727338413fcbbbe77bbb
Alek-Grujic <alek.grujic001@outlook.com>
2026-08-12 06:45:27 +0200

    refactor: manage step state with useReducer

--- src/DateCounter.jsx
+++ src/DateCounter.jsx
@@ -1,45 +1,53 @@
-import { useReducer, useState } from "react";
+import { useReducer } from "react";
+
+const initialState = {
+  count: 0,
+  step: 1,
+};
 
 function reducer(state, action) {
-  if (action.type === "inc") return state + 1;
-  if (action.type === "dec") return state - 1;
-  if (action.type === "setCount") return action.payload;
+  if (action.type === "inc")
+    return { ...state, count: state.count + state.step };
+
+  if (action.type === "dec")
+    return { ...state, count: state.count - state.step };
+
+  if (action.type === "setCount") return { ...state, count: action.payload };
+
+  if (action.type === "setStep") return { ...state, step: action.payload };
+
+  if (action.type === "reset") return initialState;
+
+  return state;
 }
 
 function DateCounter() {
-  // const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  const [count, dispatch] = useReducer(reducer, 0);
-  const [step, setStep] = useState(1);
+  const { count, step } = state;
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
   date.setDate(date.getDate() + count);
 
   const dec = function () {
-    // setCount((count) => count - 1);
-    // setCount((count) => count - step)
     dispatch({ type: "dec" });
   };
 
   const inc = function () {
-    // setCount((count) => count + 1);
-    // setCount((count) => count + step);
     dispatch({ type: "inc" });
   };
 
   const defineCount = function (e) {
-    // setCount(Number(e.target.value));
     dispatch({ type: "setCount", payload: Number(e.target.value) });
   };
 
   const defineStep = function (e) {
-    // setStep(Number(e.target.value));
+    dispatch({ type: "setStep", payload: Number(e.target.value) });
   };
 
   const reset = function () {
-    // setCount(0);
-    setStep(1);
+    dispatch({ type: "reset" });
   };
 
   return (
@@ -69,4 +77,5 @@ function DateCounter() {
     </div>
   );
 }
+
 export default DateCounter;
