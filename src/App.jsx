931f0bc457c4b6441ced613df96c7012f2daae3b
Alek-Grujic <alek.grujic001@outlook.com>
2026-08-18 16:01:14 +0200

    feat: fetch quiz questions with useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
-import { useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import heroImg from "./assets/hero.png";
 import Header from "./Header.jsx";
 import Main from "./MainSec.jsx";
 
+const initialState = {
+  questions: [],
+  status: "ready",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "loading",
+      };
+
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div className="app">
       <Header />
